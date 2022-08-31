4e441a88d99146c26dabc1fe4d3d0ca4cc690d16
Pablo Jesus <91506505+PabloJesusdaSilva@users.noreply.github.com>
2022-08-31 17:18:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1500,
+    reset: false
+});
+
+ScrollReveal().reveal('.container-post', { delay: 200 });
+ScrollReveal().reveal('.date', { delay: 400 });
+ScrollReveal().reveal('.title', { delay: 500 });
+ScrollReveal().reveal('.description', { delay: 600 });
